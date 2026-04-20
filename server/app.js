125cc8cbc5a4f9138cacbdedfeec2c88a7df5b44
thea <thsk0001@stud.kea.dk>
2026-04-20 20:08:42 +0200

    cleanup

--- server/app.js
+++ server/app.js
@@ -1,14 +1,8 @@
 import express from 'express';
-import db from './database/connection.js'
 
 const app = express();
 
 app.use(express.json());
-
-import frontpageRouter from './routers/frontpageRouter.js';
-app.use(frontpageRouter);
-
-
 
 
 const PORT = process.env.PORT ?? 8080;
